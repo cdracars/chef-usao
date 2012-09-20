7deceea78912da71ae89ca91e1a519952ced7f03
Cody Dracars <cdracars@gmail.com>
2012-09-20 15:19:46 -0500

    removed unneeded files and cleaned up code.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,14 +2,30 @@
 # Cookbook Name:: usao
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Dracars Designs
 #
 # All rights reserved - Do Not Redistribute
 #
 
-  bash "Clone the usao-main repo" do
-    code <<-EOH
-        cd /home/vagrant/
-        git clone git@bitbucket.org:cdracars/usao-main.git
-    EOH
+  execute "download-drupal-7-modules" do
+    cwd "#{ node['drupal']['dir'] }"
+    command "drush make -y --no-core --working-copy --no-gitinfofile https://raw.github.com/cdracars/d7.usao.edu/master/d7_usao_edu.build"
+    not_if do
+      File.exists?("#{ node['drupal']['dir'] }/profiles/d7_usao_edu/modules/contrib")
+    end
+  end
+
+  execute "install-drupal-7" do
+    cwd "#{ node['drupal']['dir'] }/sites/default"
+    command "drush site-install -y \
+    d7_usao_edu \
+    --site-name='#{ node['usao']['site_name'] }' \
+    --site-mail='#{ node['usao']['site_mail'] }' \
+    --account-name=#{ node['usao']['account_name'] } \
+    --account-pass=#{ node['usao']['account_pass'] } \
+    --account-mail='#{ node['usao']['account_mail'] }' \
+    --db-url=mysql://#{ node['drupal']['db']['user'] }:#{ node['drupal']['db']['password'] }@localhost/#{ node['drupal']['db']['database'] }"
+    not_if do
+      File.exists?("/var/lib/mysql/#{ node['drupal']['db']['database'] }/ctools_css_cache.frm")
+    end
   end
